2bdf65532e15399a4d64c314b5bf96288ef85571
Konstantin Fukszon <fukszon.kostya@gmail.com>
2018-01-15 15:33:39 -0800

    Add tests for GET /todos

--- server/server.js
+++ server/server.js
@@ -23,8 +23,8 @@ app.post('/todos', (req,res)=>{
 });
 
 app.get('/todos', (req, res)=>{
-    Todo.find().then((docs)=>{
-        res.send({docs});
+    Todo.find().then((todos)=>{
+        res.send({todos});
     }, (e)=>{
         res.status(400).send(e);
     });

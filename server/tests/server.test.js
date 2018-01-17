d3e7766aaa9d2e385bbb2412ce65faa5d79b35fa
Konstantin Fukszon <fukszon.kostya@gmail.com>
2018-01-16 23:24:05 -0800

    Add text cases for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,13 +1,16 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+    _id: new ObjectID(),
     text:'first test todo'
 },
 {
+    _id: new ObjectID(),
     text:'second test todo'
 }];
 
@@ -68,4 +71,29 @@ describe('GET /todos', ()=>{
             })
             .end(done);
     });
+describe('GET /todos/:id', ()=>{
+    it('Should reqturn object id', (done)=>{
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res)=>{
+                expect(res.body.todo.text).toBe(todos[0].text);
+            })
+            .end(done);
+    });
+    it('should return 404, if todo not found', (done)=>{
+        var id = new ObjectID().toHexString();
+        request(app)
+            .get(`/todos/${id}`)
+            .expect(404)
+            .end(done);
+    });
+    it('should return 404, if _id is not valid', (done)=>{
+        request(app)
+            .get(`/todos/123`)
+            .expect(404)
+            .end(done);
+    });
+});
+
 });

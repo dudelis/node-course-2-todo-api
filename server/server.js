561de485ae5e508c99e6dbd7221425e28d8bcd0e
Denallix Administrator <fukszon.kostya@gmail.com>
2018-01-27 17:20:42 -0800

    Addded PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser  = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser  = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
@@ -22,7 +23,6 @@ app.post('/todos', (req,res)=>{
         res.status(400).send(e);
     })
 });
-
 app.get('/todos', (req, res)=>{
     Todo.find().then((todos)=>{
         res.send({todos});
@@ -30,7 +30,6 @@ app.get('/todos', (req, res)=>{
         res.status(400).send(e);
     });
 });
-
 app.get('/todos/:id', (req,res)=>{
     var id = req.params.id;
     if (!ObjectID.isValid(id)){
@@ -46,7 +45,6 @@ app.get('/todos/:id', (req,res)=>{
         res.status(400).send();
     });
 });
-
 app.delete('/todos/:id', (req,res)=>{
     var id = req.params.id;
     if (!ObjectID.isValid(id)){
@@ -62,6 +60,29 @@ app.delete('/todos/:id', (req,res)=>{
         res.status(400).send();
     });
 });
+app.patch('/todos/:id', (req, res)=>{
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)){
+        return res.status(404).send();
+    };
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if (_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    } else{
+        body.completed = false;
+        body.completedAt = null;
+    };
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo)=>{
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send(todo);
+    }).catch((e)=>{
+        res.status(400).send();
+    });
+})
 
 app.listen(port, ()=>{
     console.log(`Started on port ${port}`);
